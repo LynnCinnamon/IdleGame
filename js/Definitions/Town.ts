e786054d0d15a00c7bc048b8041ec38bd9dcfdac
Lynn Schallenberg <Schallenberg0815@gmail.com>
2021-11-08 16:29:08 +0100

    Moving to typescript

---
+++ js/Definitions/Town.ts
@@ -0,0 +1,39 @@
+/// <reference path="../../node_modules/@types/knockout/index.d.ts" />
+/// <reference path="./Action.ts" />
+
+var allTowns: { [s: string]: Town; } = {};
+
+
+class Town {
+    name: string;
+    locked: KnockoutObservable<boolean>;
+    actions: KnockoutObservableArray<BaseAction>;
+    progress: KnockoutObservableArray<Progress>;
+    constructor(name, actionNames, progressNames) {
+        var self = this;
+
+        self.name = name;
+
+        self.locked = ko.observable(true);
+
+        self.actions = ko.observableArray([]);
+        actionNames.forEach((name) => {
+            self.actions.push(allActions[name]);
+        });
+        self.progress = ko.observableArray([]);
+        progressNames.forEach((name) => {
+            if (allProgress[name]) {
+                self.progress.push(allProgress[name]);
+            }
+
+            else {
+                console.log(allProgress);
+                console.log(name);
+                console.log(allProgress[name]);
+            }
+        });
+
+
+        allTowns[self.name] = self;
+    }
+}
